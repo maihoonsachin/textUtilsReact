f4656bc2d5a18385546eefc934c9cc1ec08b69b6
sachinverma271855@gmail.com <sachinverma271855@gmail.com>
2026-04-04 18:46:50 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -4,29 +4,86 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
+import { useEffect } from 'react'; 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
-function App() {
+function App() 
+{
   const[mode,setMode] = useState('light');
+  const[alert,setAlert] = useState(null);
+  const[myColor, setMyColor] = useState('#042743');
+
+  useEffect(() => {
+    if (mode === 'dark') {
+      document.body.style.backgroundColor = myColor;
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      document.body.style.backgroundColor = 'white';
+      document.title = "TextUtils - Light Mode";
+    }
+  }, [mode, myColor]);
+
 
   const toggleMode = () => {
-    if(mode === 'light'){
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = '#042743'; 
-    }
-    else{
+      updateAlert("Dark mode has been enabled", "success");
+    } else {
       setMode('light');
-      document.body.style.backgroundColor = 'white';
+      updateAlert("Light mode has been enabled", "success");
+    }
+  };
 
+  const updateAlert = (message, type) => {
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const chooseColor = (c) => {
+    setMyColor(c);
+    if(mode === 'dark'){
+      document.body.style.backgroundColor = myColor;
     }
   }
+
+  // const toggleMode = () => {
+  //   if(mode === 'light'){
+  //     setMode('dark');
+  //     document.body.style.backgroundColor = myColor;
+  //     updateAlert("Dark mode has been enabled ","success");
+  //   }
+  //   else{
+  //     setMode('light');
+  //     document.body.style.backgroundColor = 'white';
+  //     updateAlert("Light mode has been enabled ","successs")
+  //   }
+  // }
   return (
     <> 
       {/* <Navbar title="TextUtils" aboutText="About textUtils"/> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <div className="container">
-        <TextForm heading="Enter the Text to analyze" mode={mode}/>
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} aboutText="About" toggleMode={toggleMode} chooseColor={chooseColor}/>
+        <Alert alert={alert} />
+        <div className="container"> 
+          <Routes>
+              <Route exact path="/about" element = {<About/>}/>
+
+              <Route exact path="/" 
+              element = {<TextForm updateAlert={updateAlert} myColor={myColor} 
+              chooseColor={chooseColor}heading="Enter the Text to analyze" mode={mode}/>}/>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,11 +13,13 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         // console.log("Uppercase was clicked");
         setText(text.toUpperCase());
+        props.updateAlert("Converted to uppercase","success");
     } 
 
     const handleDownClick = () => {
         // console.log("Lowercase was clicked");
         setText(text.toLowerCase());
+        props.updateAlert("Converted to lowercase","success");
     }
 
     const handleOnChange = (event) =>{
@@ -28,32 +30,36 @@ export default function TextForm(props) {
     const findWord = () => {
         let words = text.split(" ");
         if(words.includes(searchWord)){
-            alert("Word found in the text");
+            props.updateAlert("Word found in the text","success");
+            // alert("Word found in the text");
         }
         else{
-            alert("Word not found in the text");
+            props.updateAlert("Word not found in the text","danger");
+            // alert("Word not found in the text");
         } 
     }
 
     const handleCopy = () => {
-        console.log("I am copy");
+        // console.log("I am copy");
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.updateAlert("Text copied to clipboard","success");
     }
 
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.updateAlert("Extra spaces removed","success");
     }
 
 
     return (
         <>
-            <div className="container" style={{ backgroundColor: props.mode === 'light' ? 'white' : '#042743', color: props.mode === 'light' ? 'black' : 'white' }}> 
+            <div className="container" style={{ backgroundColor: props.mode === 'light' ? 'white' : props.myColor, color: props.mode === 'light' ? 'black' : 'white' }}> 
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} style={{ backgroundColor: props.mode === 'light' ? 'white' : '#042743', color: props.mode === 'light' ? 'black' : 'white' }} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} style={{ backgroundColor: props.mode === 'light' ? 'white' : props.myColor, color: props.mode === 'light' ? 'black' : 'white' }} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
                 <input 
                     type="text"
@@ -62,17 +68,33 @@ export default function TextForm(props) {
                     placeholder="Enter word to search"
                     value={searchWord}
                     onChange={handleSearchChange}
-                    style={{ backgroundColor: props.mode === 'light' ? 'white' : '#042743', color: props.mode === 'light' ? 'black' : 'white' }}
+                    style={{ backgroundColor: props.mode === 'light' ? 'white' : props.myColor, color: props.mode === 'light' ? 'black' : 'white' }}
                 />
-                <button className="btn btn-primary mx-2 " onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2 " onClick={handleDownClick}>Convert to LowerCase</button>
-                <button className="btn btn-primary mx-2 " onClick={findWord}>Search</button>
-                <button className="btn btn-primary mx-2 " onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-2 " onClick={handleExtraSpaces}>Remove Extra Spaces</button> 
+                <button className="btn btn mx-2 " onClick={handleUpClick} style={{ 
+                    backgroundColor: props.mode === 'light' ? 'white' : props.myColor, 
+                    color: props.mode === 'light' ? 'black' : 'white', 
+                    border: `1px solid ${props.mode === 'light' ? 'black' : 'white'}`}}>Convert to Uppercase</button>
+                <button className="btn btn mx-2 " onClick={handleDownClick} style={{ 
+                    backgroundColor: props.mode === 'light' ? 'white' : props.myColor, 
+                    color: props.mode === 'light' ? 'black' : 'white',
+                    border: `1px solid ${props.mode === 'light' ? 'black' : 'white'}`}}>Convert to LowerCase</button>
+                <button className="btn btn mx-2 " onClick={findWord} style={{ 
+                    backgroundColor: props.mode === 'light' ? 'white' : props.myColor, 
+                    color: props.mode === 'light' ? 'black' : 'white',
+                    border: `1px solid ${props.mode === 'light' ? 'black' : 'white'}`}}>Search</button>
+                <button className="btn btn mx-2 " onClick={handleCopy} style={{ 
+                    backgroundColor: props.mode === 'light' ? 'white' : props.myColor, 
+                    color: props.mode === 'light' ? 'black' : 'white',
+                    border: `1px solid ${props.mode === 'light' ? 'black' : 'white'}`,
+                    border: `1px solid ${props.mode === 'light' ? 'black' : 'white'}`}}>Copy Text</button>
+                <button className="btn btn mx-2 " onClick={handleExtraSpaces} style={{ 
+                    backgroundColor: props.mode === 'light' ? 'white' : props.myColor, 
+                    color: props.mode === 'light' ? 'black' : 'white',
+                    border: `1px solid ${props.mode === 'light' ? 'black' : 'white'}`}}>Remove Extra Spaces</button> 
             </div>
-            <div className="container my-2" style={{ backgroundColor: props.mode === 'light' ? 'white' : '#042743', color: props.mode === 'light' ? 'black' : 'white' }}>
+            <div className="container my-2" style={{ backgroundColor: props.mode === 'light' ? 'white' : props.myColor, color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h2>Your Text Summary</h2>
-                <p> {text.trim().split(" ").length} words and {text.length} characters</p>
+                <p> {(text.trim() === "") ? 0 : text.trim().split(" ").length} words and {text.length} characters</p>
                 <p>Average time to read the content: {(text.split(" ").length)*0.008}</p> 
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Write something for preview in the above"}</p>
